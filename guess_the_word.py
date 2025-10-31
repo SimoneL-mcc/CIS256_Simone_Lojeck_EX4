d5a6f7854dc33ff7015e375ae0fcc322dab2321d
SimoneL-mcc <SIM2142572@maricopa.edu>
2025-10-30 22:05:11 -0700

    SL: Refinement of code - Updated header - Removed test print statements. - Added spaces or characters to clarify output. - Rearanged code segments & added notes for expansion

--- guess_the_word.py
+++ guess_the_word.py
@@ -5,26 +5,30 @@
 
 import random
 
-# Welcome message
-print('*' * 40)
-print("   Welcome to the Guess The Word Game!  ")
-print("  the topic is Space Shuttle Names")
-print('*' * 40)
-
 # Define word list
 words = ['Enterprise', 'Columbia', 'Challenger', 'Discovery',
          'Atlantis', 'Endeavour']
-
-# Set # of guesses
-num_guesses = 20
-print(f"You have {num_guesses} chances to guess the name.")
+topic = 'Space Shuttle Names'
+# Expansion: include other topics & word lists..possibly as a dictionary or other structure
 
 # select random word
 word = random.choice(words)
+# Expansion: random choice of topic, and then word from topic's list
 
+# Welcome message
+print('*' * 40)
+print("   Welcome to the Guess The Word Game!  ")
+print(f"  the topic is {topic}")
+print('*' * 40)
+
+# Set # of guesses
+num_guesses = 20
+print(f"--> You have {num_guesses} chances to guess the name.")
+
+# Loop through for each guess
 index = 0 # starting index
 while num_guesses > 0:
-    num_guesses -= 1 # reduce number
+    num_guesses -= 1 # reduce number of guesses
 
     # Prompt user to guess one letter at a time
     guess = input(f"\nGuess the letter of the word: ").lower()
@@ -35,17 +39,16 @@ while num_guesses > 0:
 
         # If final letter in word, They won!
         if index == len(word) - 1:
-            print(f" ***  CONGRATULATIONS!!! YOU WON!!  The ship was {word}")    # end game
+            print(f"\n\n ***  CONGRATULATIONS!!! YOU WON!!  The ship was {word}")    # end game
             break # end game
         else:
             index += 1  # Increment to next letter
-            print(f"index = {index}")
 
     # If incorrect, note wrong letter
     else:
-        print(f"Sorry, {guess} is not the next letter.  You have {num_guesses} guesses left.")
+        print(f"Sorry, '{guess}' is not the next letter.  You have {num_guesses} guesses left.")
 
         # If they are out of guesses, They lost
         if num_guesses == 0:
-            print(f" ***  GAME OVER....You are out of guesses. The ship was '{word}'") # end game
+            print(f"\n\n ***  GAME OVER....You are out of guesses. The ship was '{word}'") # end game
 
