3d200ebc857852be6c27eb9b68ad072d133cb07a
SimoneL-mcc <SIM2142572@maricopa.edu>
2025-10-30 17:22:00 -0700

    Added algorithm.

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,5 +1,25 @@
 # Simone Lojeck
 # 10/30/2025
 # CIS256 33284, Fall 2025
-# Exercise 4.0 (EX4)
+# Exercise 4.0 (EX4) - Game to guess
+
+# Define word list
+
+# Set # of guesses
+
+# select random word
+
+# Prompt user to guess one letter at a time
+
+# Results of user's guess
+
+    # If correct, reveal letter
+
+        # If final letter in word, They won!
+        # end game
+
+    # If incorrect, note wrong letter
+
+        # If they are out of guesses, They lost
+        # end game
 
