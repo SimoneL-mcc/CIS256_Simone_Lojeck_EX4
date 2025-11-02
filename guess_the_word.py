7c1762f5ed7ada7a1b8289927161ab0965a59f82
SimoneL-mcc <SIM2142572@maricopa.edu>
2025-11-01 20:13:38 -0700

    SL: Modified file for use with pytest

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,9 +1,42 @@
 # Simone Lojeck
 # 10/30/2025
 # CIS256 33284, Fall 2025
-# Exercise 4.0 (EX4) - Game to guess
+# Exercise 4.0 (EX4) - Game to guess a word
 
 import random
+
+def select_word(words):
+    """ Function: Randomly select a word from inputted list of words """
+    # select random word
+    word = random.choice(words)
+    # Expansion: random choice of topic, and then word from topic's list
+    return word
+
+def process_guess(word, index, guess, num_guesses):
+    """ Function: Process the current guess """
+    endgame = False
+
+    # If correct, reveal letter
+    if (guess == word[index].lower()):
+        print(f"Correct! '{guess}' is the next letter!!  You have {num_guesses} guesses left.")
+
+        # If final letter in word, They won!
+        if index == len(word) - 1:
+            print(f"\n\n ***  CONGRATULATIONS!!! YOU WON!!  The ship was {word}")    # end game
+            endgame = True # end game
+        else:
+            index += 1  # Increment to next letter
+
+    # If incorrect, note wrong letter
+    else:
+        print(f"Sorry, '{guess}' is not the next letter.  You have {num_guesses} guesses left.")
+
+        # If they are out of guesses, They lost
+        if num_guesses == 0:
+            print(f"\n\n ***  GAME OVER....You are out of guesses. The ship was '{word}'")
+            endgame = True # end game
+
+    return index, num_guesses, endgame
 
 # Define word list
 words = ['Enterprise', 'Columbia', 'Challenger', 'Discovery',
@@ -11,9 +44,8 @@ words = ['Enterprise', 'Columbia', 'Challenger', 'Discovery',
 topic = 'Space Shuttle Names'
 # Expansion: include other topics & word lists..possibly as a dictionary or other structure
 
-# select random word
-word = random.choice(words)
-# Expansion: random choice of topic, and then word from topic's list
+# Select word for this game
+word = select_word(words)
 
 # Welcome message
 print('*' * 40)
@@ -27,28 +59,16 @@ print(f"--> You have {num_guesses} chances to guess the name.")
 
 # Loop through for each guess
 index = 0 # starting index
+endgame = False
 while num_guesses > 0:
     num_guesses -= 1 # reduce number of guesses
 
     # Prompt user to guess one letter at a time
     guess = input(f"\nGuess the letter of the word: ").lower()
 
-    # If correct, reveal letter
-    if (guess == word[index].lower()):
-        print(f"Correct! '{guess}' is the next letter!!  You have {num_guesses} guesses left.")
+    # Process the guess
+    index, num_guesses, endgame = process_guess(word, index, guess, num_guesses)
 
-        # If final letter in word, They won!
-        if index == len(word) - 1:
-            print(f"\n\n ***  CONGRATULATIONS!!! YOU WON!!  The ship was {word}")    # end game
-            break # end game
-        else:
-            index += 1  # Increment to next letter
-
-    # If incorrect, note wrong letter
-    else:
-        print(f"Sorry, '{guess}' is not the next letter.  You have {num_guesses} guesses left.")
-
-        # If they are out of guesses, They lost
-        if num_guesses == 0:
-            print(f"\n\n ***  GAME OVER....You are out of guesses. The ship was '{word}'") # end game
-
+    # Break while loop if game is over
+    if endgame:
+        break
